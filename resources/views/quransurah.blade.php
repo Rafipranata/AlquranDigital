60ba8124d3820eca6104680ceb265f5d166c8a17
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2024-01-25 00:39:08 +0700

    Update

--- resources/views/quransurah.blade.php
+++ resources/views/quransurah.blade.php
@@ -4,50 +4,45 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Amiri+Quran&family=Aref+Ruqaa&display=swap" rel="stylesheet">
+    <title>AlquranDigital</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amiri+Quran&family=Aref+Ruqaa&display=swap" rel="stylesheet">
     <style>
-        td {
+        td,
+        th {
+            border-bottom: none;
             font-family: 'Amiri Quran', serif;
         }
-    </style>
 
+        .ayat {
+            float: right;
+            font-size: 25px;
+        }
+    </style>
 </head>
 
 <body>
-
     <div class="container">
-
-
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">ayat</th>
-                    <th scope="col">Surah</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                {{-- blade templating => foreach loop --}}
-                @foreach ($response->ayat as $ayat)
-                <tr>
-                    <td>{{ $loop->iteration}}</td>
-                    <td>{{ $ayat->ar }}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-
+        <div class="table-responsive">
+            <table class="table" style="border: none;">
+                <tbody>
+                    {{-- blade templating => foreach loop --}}
+                    @foreach ($response->ayat as $ayat)
+                        <th>
+                            <td class="ayat text-right">{{ $ayat->ar }}</td>
+                        </th>
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td class="arti text-left">{{ $ayat->idn }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
