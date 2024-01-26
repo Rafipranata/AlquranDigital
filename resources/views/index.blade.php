036947b0372be8b0d14e9b0256fd15c8586cddcc
ATHAANGGORO10 <atha.anggoro2019@gmail.com>
2024-01-27 06:00:26 +0700

    Update New Fitur Toggle-Mode, Arrow, Navbar, & Btn-Tafsir.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,14 +15,44 @@
         td {
             font-family: 'Amiri Quran', serif;
         }
+
+        :root {
+            --Colo-default-6: #280274;
+            --Colo-default-5: #FE7A36;
+            --Colo-default-4: #E9F6FF;
+            --Color-default-3: #8a2be2;
+            --Color-default-2: #6501c3;
+            --Color-default-1: #ffffff;
+            --Font-default-3: sans-serif;
+        }
+
+        .navbar {
+            transition: transform 0.3s ease-in-out;
+            background-color: var(--Colo-default-4);
+        }
+
+        .navbar .container .table {
+            margin-top: 25px;
+        }
+
+        .navbar .navbar-brand {
+            color: var(--Colo-default-5);
+        }
+
+        .navbar-brand-fo {
+            color: var(--Colo-default-6);
+        }
     </style>
 
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg" style="background-color: #E9F6FF ">
+    <nav class="navbar navbar-expand-lg shadow-sm p-2">
         <div class="container">
-            <a class="navbar-brand fw-bold" href="https://www.instagram.com/duta.code/?igsh=NzZnOTIwa3Y2ZDYz" style="color: #FE7A36;">Duta  <span style="color: #280274;">Quran</span></a>
+            <a class="navbar-brand fw-bold fs-2" href="https://www.instagram.com/duta.code/?igsh=NzZnOTIwa3Y2ZDYz">
+                Duta
+                <span class="navbar-brand-fo">Quran</span>
+            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -38,32 +68,33 @@
             </div>
         </div>
     </nav>
-
-    <div class="container">
-        @foreach ($response as $item)
-            <div class="card mt-3 mb-3 shadow-sm ">
-                <div class="card-body">
-                    <div class="float-start fw-bold">
-                        <!-- Content floated to the right -->
-                        <span class="fs-5">
-                            {{ $item->nama_latin }}
-                        </span>
-                        <br>
-                        <i>
-                            {{ $item->arti }}
-                        </i>
-                    </div>
-                    <div class="float-end fs-3">
-                        <!-- Content floated to the left -->
-                        <div>
-                            <a href="/quran/surah/{{ $item->nomor }}">
-                                <i class="bi bi-arrow-right"></i>
-                            </a>
+    <div class="section">
+        <div class="container">
+            @foreach ($response as $item)
+                <div class="card mt-3 mb-3 shadow-sm ">
+                    <div class="card-body">
+                        <div class="float-start fw-bold">
+                            <!-- Content floated to the right -->
+                            <span class="fs-5">
+                                {{ $item->nama_latin }}
+                            </span>
+                            <br>
+                            <i>
+                                {{ $item->arti }}
+                            </i>
+                        </div>
+                        <div class="float-end fs-3">
+                            <!-- Content floated to the left -->
+                            <div>
+                                <a href="/quran/surah/{{ $item->nomor }}">
+                                    <i class="bi bi-arrow-right"></i>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
